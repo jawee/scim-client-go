6cf90730d71f172b4dba6c9bed3126c41c11aff3
Andreas Olsson <olsson.andreas@gmail.com>
2023-08-31 17:01:20 +0200

    things

--- internal/main-func.go
+++ internal/main-func.go
@@ -1,7 +1,49 @@
 package internal
 
-import "fmt"
+import (
+    "reflect"
+	"fmt"
+	"strconv"
+)
 
 func MainFunc() {
     fmt.Printf("Hello internal\n")
+    asdf()
 }
+
+type FooBar struct {
+	Foo string
+	Bar int
+}
+
+func asdf() {
+	structValue := FooBar{Foo: "foo", Bar: 10}
+	fields := reflect.TypeOf(structValue)
+	values := reflect.ValueOf(structValue)
+
+	num := fields.NumField()
+
+	for i := 0; i < num; i++ {
+		field := fields.Field(i)
+		value := values.Field(i)
+		fmt.Print("Type:", field.Type, ",", field.Name, "=", value, "\n")
+
+		switch field.Type.Kind() {
+		case reflect.String:
+			v := value.String()
+			fmt.Print(v, "\n")
+		case reflect.Int:
+			v := strconv.FormatInt(value.Int(), 10)
+			fmt.Print(v, "\n")
+		case reflect.Int32:
+			v := strconv.FormatInt(value.Int(), 10)
+			fmt.Print(v, "\n")
+		case reflect.Int64:
+			v := strconv.FormatInt(value.Int(), 10)
+			fmt.Print(v, "\n")
+		default:
+			panic("Not support type of struct")
+		}
+	}
+}
+
