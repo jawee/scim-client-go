5c3bcd907b11fe0a0bebfa913e18e1105bd164d3
Andreas Olsson <olsson.andreas@gmail.com>
2023-09-08 17:47:58 +0200

    User comparison

--- cmd/scim-client-go/main.go
+++ cmd/scim-client-go/main.go
@@ -1,9 +1,88 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jawee/scim-client-go/internal"
 )
 
 func main() {
     internal.MainFunc()
+
+    user1 := User {
+        Id: "asdf",
+        UserName: "some.user@company.name",
+        Email: "some.user@company.name",
+        Department: "clown",
+        PhoneNumber: "12345678",
+        FirstName: "Some",
+        LastName: "User",
+        Active: true,
+        ExternalId: "",
+    }
+
+    user2 := User {
+        Id: "asdf",
+        UserName: "some@user.name",
+        Email: "some@user.name",
+        Department: "clown",
+        PhoneNumber: "12345678",
+        FirstName: "Some",
+        LastName: "User",
+        Active: true,
+        ExternalId: "",
+    }
+
+    res := GetDiff(user1, user2)
+
+    fmt.Printf("%v\n", res)
+}
+
+func GetDiff(user1, user2 User) []string {
+    result := []string{}
+    if user1.Id != user2.Id {
+        result = append(result, "Id")
+    }
+
+    if user1.UserName != user2.UserName {
+        result = append(result, "UserName")
+    }
+
+    if user1.Email != user2.Email {
+        result = append(result, "Email")
+    }
+    
+    if user1.Department != user2.Department {
+        result = append(result, "Department")
+    }
+
+    if user1.PhoneNumber != user2.PhoneNumber {
+        result = append(result, "PhoneNumber")
+    }
+
+    if user1.FirstName != user2.FirstName {
+        result = append(result, "FirstName")
+    }
+
+    if user1.LastName != user2.LastName {
+        result = append(result, "LastName")
+    }
+
+    if user1.Active != user2.Active {
+        result = append(result, "Active")
+    }
+
+    return result
+}
+
+type User struct {
+    Id string
+    UserName string
+    Email string
+    PhoneNumber string
+    Department string
+    FirstName string
+    LastName string
+    Active bool
+    ExternalId string
 }
--- internal/main-func.go
+++ internal/main-func.go
@@ -2,48 +2,53 @@ package internal
 
 import (
     "reflect"
-	"fmt"
-	"strconv"
+    "fmt"
 )
 
 func MainFunc() {
-    fmt.Printf("Hello internal\n")
+    // fmt.Printf("Hello internal\n")
     asdf()
 }
 
 type FooBar struct {
-	Foo string
-	Bar int
+    Foo string
+    Bar int
 }
 
 func asdf() {
-	structValue := FooBar{Foo: "foo", Bar: 10}
-	fields := reflect.TypeOf(structValue)
-	values := reflect.ValueOf(structValue)
+    structValue := FooBar{Foo: "foo", Bar: 10}
+    fields := reflect.TypeOf(structValue)
+    values := reflect.ValueOf(structValue)
 
-	num := fields.NumField()
+    for i := 0; i < fields.NumField(); i++ {
+        field := fields.Field(i)
+        value := values.Field(i)
+        fmt.Printf("Type: %v, Name: %v, Value: %v\n", field.Type, field.Name, value)
+    }
 
-	for i := 0; i < num; i++ {
-		field := fields.Field(i)
-		value := values.Field(i)
-		fmt.Print("Type:", field.Type, ",", field.Name, "=", value, "\n")
-
-		switch field.Type.Kind() {
-		case reflect.String:
-			v := value.String()
-			fmt.Print(v, "\n")
-		case reflect.Int:
-			v := strconv.FormatInt(value.Int(), 10)
-			fmt.Print(v, "\n")
-		case reflect.Int32:
-			v := strconv.FormatInt(value.Int(), 10)
-			fmt.Print(v, "\n")
-		case reflect.Int64:
-			v := strconv.FormatInt(value.Int(), 10)
-			fmt.Print(v, "\n")
-		default:
-			panic("Not support type of struct")
-		}
-	}
+    // num := fields.NumField()
+    //
+    // for i := 0; i < num; i++ {
+    //     field := fields.Field(i)
+    //     value := values.Field(i)
+    //     fmt.Print("Type:", field.Type, ",", field.Name, "=", value, "\n")
+    //
+    //     switch field.Type.Kind() {
+    //         case reflect.String:
+    //             v := value.String()
+    //             fmt.Print(v, "\n")
+    //         case reflect.Int:
+    //             v := strconv.FormatInt(value.Int(), 10)
+    //             fmt.Print(v, "\n")
+    //         case reflect.Int32:
+    //             v := strconv.FormatInt(value.Int(), 10)
+    //             fmt.Print(v, "\n")
+    //         case reflect.Int64:
+    //             v := strconv.FormatInt(value.Int(), 10)
+    //             fmt.Print(v, "\n")
+    //         default:
+    //             panic("Not support type of struct")
+    //     }
+    // }
 }
 
