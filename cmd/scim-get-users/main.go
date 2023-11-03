2c60de29f0316981b4caf415889291226b89b3eb
Andreas Olsson <olsson.andreas@gmail.com>
2023-11-03 17:46:36 +0100

    more messing around

---
+++ cmd/scim-get-users/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+
+func main() {
+    sourceUsers := getSourceUsers()
+
+    toCreate := make([]User, 0)
+    toUpdate := make([]User, 0)
+
+    for _, v := range sourceUsers {
+        _, err := getDbUserById(v.Id)
+        if err != nil {
+            toCreate = append(toCreate, v)
+            continue
+        }
+        //TODO: Check diff between existing and new
+        toUpdate = append(toUpdate, v)
+    }
+
+    for _, v := range toCreate {
+        log.Printf("ToCreate: %v\n", v)
+    }
+
+    for _, v := range toUpdate {
+        log.Printf("ToUpdate: %v\n", v)
+    }
+}
+
+func getDbUserById(id string) (*User, error) {
+    users := []User{
+        {
+            Id: "1",
+            UserName: "some.user@company.name",
+            Email: "some.user@company.name",
+            Department: "clown",
+            PhoneNumber: "12345678",
+            FirstName: "Some",
+            LastName: "User",
+            Active: true,
+            ExternalId: "",
+        },
+        {
+            Id: "2",
+            UserName: "other.user@company.anem",
+            Email: "other.user@company.name",
+            Department: "jester",
+            PhoneNumber: "87654321",
+            FirstName: "Other",
+            LastName: "User",
+            Active: true,
+            ExternalId: "",
+        },
+    }
+
+    var res *User
+    for _, v := range users {
+        if v.Id == id {
+            res = &v
+            break
+        }
+    }
+
+    if res == nil {
+        return nil, fmt.Errorf("Couldn't find user\n")
+    }
+
+    return res, nil
+}
+
+func getSourceUsers() []User {
+    users := []User{
+        {
+            Id: "1",
+            UserName: "some.user@company.name",
+            Email: "some.user@company.name",
+            Department: "clown",
+            PhoneNumber: "12345678",
+            FirstName: "Some",
+            LastName: "User",
+            Active: true,
+            ExternalId: "",
+        },
+        {
+            Id: "2",
+            UserName: "other.user@company.anem",
+            Email: "other.user@company.name",
+            Department: "jester",
+            PhoneNumber: "87654321",
+            FirstName: "Other",
+            LastName: "User",
+            Active: true,
+            ExternalId: "",
+        },
+        {
+            Id: "3",
+            UserName: "third.user@company.anem",
+            Email: "third.user@company.name",
+            Department: "jester",
+            PhoneNumber: "87654321",
+            FirstName: "Other",
+            LastName: "User",
+            Active: true,
+            ExternalId: "",
+        },
+    }
+    return users
+}
+
+type User struct {
+    Id string
+    UserName string
+    Email string
+    PhoneNumber string
+    Department string
+    FirstName string
+    LastName string
+    Active bool
+    ExternalId string
+}
