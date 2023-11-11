baabd0d4c371a67a830973cc5b361910935c30d9
Andreas Olsson <olsson.andreas@gmail.com>
2023-11-11 08:37:16 +0100

    some json structs for scim-api

---
+++ internal/scim-api/requests.go
@@ -0,0 +1,23 @@
+package scimapi
+
+type PostUserRequest struct {
+	UserName       string         `json:"userName"`
+	Active         bool           `json:"active"`
+	DisplayName    string         `json:"displayName"`
+	Schemas        []string       `json:"schemas"`
+	ExternalID     string         `json:"externalId"`
+	Name           Name           `json:"name"`
+	Emails         []Emails       `json:"emails"`
+	EnterpriseUser EnterpriseUser `json:"urn:ietf:params:scim:schemas:extension:enterprise:2.1:User"`
+}
+
+type PatchRequest struct {
+	Schemas    []string     `json:"schemas"`
+	Operations []Operations `json:"Operations"`
+}
+
+type Operations struct {
+	Op    string `json:"op"`
+	Path  string `json:"path"`
+	Value string `json:"value"`
+}
---
+++ internal/scim-api/responses.go
@@ -0,0 +1,65 @@
+package scimapi
+
+import "time"
+
+type GetUsersResponse struct {
+    Schemas      []string    `json:"schemas"`
+    TotalResults int         `json:"totalResults"`
+    ItemsPerPage int         `json:"itemsPerPage"`
+    StartIndex   int         `json:"startIndex"`
+    Resources    []Resources `json:"Resources"`
+}
+
+type Emails struct {
+    Type    string `json:"type"`
+    Primary bool   `json:"primary"`
+    Value   string `json:"value"`
+}
+
+type Meta struct {
+    ResourceType string    `json:"resourceType"`
+    Created      time.Time `json:"created"`
+    LastModified time.Time `json:"lastModified"`
+}
+
+type Name struct {
+    Formatted  string `json:"formatted"`
+    FamilyName string `json:"familyName"`
+    GivenName  string `json:"givenName"`
+}
+
+type EnterpriseUser struct {
+	Manager    Manager `json:"manager"`
+	Department string  `json:"department"`
+}
+
+type Resources struct {
+    EnterpriseUser EnterpriseUser `json:"urn:ietf:params:scim:schemas:extension:enterprise:2.1:User"`
+    Active         bool           `json:"active"`
+    DisplayName    string         `json:"displayName"`
+    Emails         []Emails       `json:"emails"`
+    Meta           Meta           `json:"meta"`
+    Name           Name           `json:"name"`
+    UserName       string         `json:"userName"`
+    ExternalID     string         `json:"externalId"`
+    ID             string         `json:"id"`
+    Schemas        []string       `json:"schemas"`
+}
+
+type PostResponse struct {
+	EnterpriseUser EnterpriseUser `json:"urn:ietf:params:scim:schemas:extension:enterprise:2.1:User"`
+	Active         bool           `json:"active"`
+	DisplayName    string         `json:"displayName"`
+	Emails         []Emails       `json:"emails"`
+	Meta           Meta           `json:"meta"`
+	Name           Name           `json:"name"`
+	UserName       string         `json:"userName"`
+	ExternalID     string         `json:"externalId"`
+	ID             string         `json:"id"`
+	Schemas        []string       `json:"schemas"`
+}
+
+type Manager struct {
+	Value string `json:"value"`
+}
+
