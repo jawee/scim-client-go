4b8f592484874ec1465865304df8ccad69c4644f
Andreas Olsson <olsson.andreas@gmail.com>
2023-11-26 06:31:36 +0100

    refactoring

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -2,17 +2,11 @@ package configuration
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
-	"os"
-	"path"
 )
 
 type ConfigurationProvider interface {
     GetConfigurationJson() ([]byte, error)
-}
-
-type FileConfigurationProvider struct {
 }
 
 type DestinationConfig struct {
@@ -27,48 +21,6 @@ type Config struct {
     DestinationConfig DestinationConfig `json:"destinationconfig,omitempty"`
     ReaderConfig ReaderConfig `json:"readerconfig"`
 }
-
-func findConfigurationFile() (*os.File, error) {
-    // userCfgDir, err := os.UserConfigDir();
-    cwd, err := os.Getwd()
-    if err != nil {
-        log.Printf("FileConfigurationProvider.findConfigurationFile. Couldn't get cwd\n")
-        return nil, err
-    }
-
-    configDir := "/configs"
-    path := path.Join(cwd, configDir, "config.json")
-    log.Printf("FileConfigurationProvider.findConfigurationFile. Checking if %s exists\n", path)
-
-    file, err := os.Open(path)
-    if err != nil {
-        log.Printf("FileConfigurationProvider.findConfigurationFile. Error opening configuration file: %s\n", err)
-        return nil, err
-    }
-
-    log.Printf("FileConfigurationProvider.findConfigurationFile. File found at %s.\n", path)
-    return file, nil
-}
-
-func (f *FileConfigurationProvider) GetConfigurationJson() ([]byte, error) {
-    file, err := findConfigurationFile()
-    if err != nil {
-        log.Printf("Couldn't find configuration file. %s\n", err)
-        return nil, fmt.Errorf("Couldn't find configuration file. %s\n", err)
-    }
-
-    bytes := make([]byte, 1024)
-
-    readTotal, err := file.Read(bytes)
-
-    if err != nil {
-        log.Printf("FileConfigurationProvider.GetConfigurationJson. Error reading configuration file: %s\n", err)
-        return nil, err
-    }
-    
-    return bytes[:readTotal], nil
-}
-
 
 func New(configProvider ConfigurationProvider) (*Config, error) {
     log.Println("New. Loading configuration")    
---
+++ internal/configuration/fileconfigurationprovider.go
@@ -0,0 +1,55 @@
+package configuration
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
+
+type FileConfigurationProvider struct {
+}
+
+func findConfigurationFile() (*os.File, error) {
+    // userCfgDir, err := os.UserConfigDir();
+    cwd, err := os.Getwd()
+    if err != nil {
+        log.Printf("FileConfigurationProvider.findConfigurationFile. Couldn't get cwd\n")
+        return nil, err
+    }
+
+    configDir := "/configs"
+    path := path.Join(cwd, configDir, "config.json")
+    log.Printf("FileConfigurationProvider.findConfigurationFile. Checking if %s exists\n", path)
+
+    file, err := os.Open(path)
+    if err != nil {
+        log.Printf("FileConfigurationProvider.findConfigurationFile. Error opening configuration file: %s\n", err)
+        return nil, err
+    }
+
+    log.Printf("FileConfigurationProvider.findConfigurationFile. File found at %s.\n", path)
+    return file, nil
+}
+
+func (f *FileConfigurationProvider) GetConfigurationJson() ([]byte, error) {
+    file, err := findConfigurationFile()
+    if err != nil {
+        log.Printf("Couldn't find configuration file. %s\n", err)
+        return nil, fmt.Errorf("Couldn't find configuration file. %s\n", err)
+    }
+
+    bytes := make([]byte, 1024)
+
+    readTotal, err := file.Read(bytes)
+
+    if err != nil {
+        log.Printf("FileConfigurationProvider.GetConfigurationJson. Error reading configuration file: %s\n", err)
+        return nil, err
+    }
+    
+    return bytes[:readTotal], nil
+}
+
+
