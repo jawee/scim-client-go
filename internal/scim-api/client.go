b5cb1b3412e1f568a8b5dec45f607c0f7586a702
Andreas Olsson <olsson.andreas@gmail.com>
2023-11-29 16:59:51 +0100

    cleaning up client.go

--- internal/scim-api/client.go
+++ internal/scim-api/client.go
@@ -57,27 +57,28 @@ type getTokenResponse struct {
 	Token string `json:"token"`
 }
 
-func HandleUser(newUser *models.User, oldUser *models.User) (ExternalId, error) {
-    token, err := getToken()
+func getExistingUser(token, userName string) (*models.User, error) {
+    url := fmt.Sprintf("%s/users/?filter=userName+eq+%s", API_URL, userName)
+
+    body, err := makeRequest(token, url, http.MethodGet)
     if err != nil {
-        log.Printf("Error: %s\n", err)
-        return ERROR_EXTERNAL_ID, err
+        return nil, err
     }
-    log.Printf("Got token: '%s'\n", token)
-    // requestURL := fmt.Sprintf("http://DESKTOP-QQQIEJC.local:59629/scim/users")
-    // requestURL := fmt.Sprintf("http://localhost:5000/scim/token")
-    requestURL := fmt.Sprintf("%s/users", API_URL)
-	// resp, err := http.Get(requestURL)
- //    if err != nil {
- //        log.Printf("Error: %s\n", err)
- //        return ERROR_EXTERNAL_ID, err
- //    }
-    
+
+    var userResponse GetUsersResponse
+    err = json.Unmarshal(body, &userResponse)
+    if err != nil {
+        return nil, err
+    }
+    return nil, nil
+}
+
+func makeRequest(token, url, method string)  ([]byte, error) {
     var body io.Reader
-    request, err := http.NewRequest(http.MethodGet, requestURL, body)
+    request, err := http.NewRequest(method, url, body)
     if err != nil {
         log.Printf("Error: %s\n", err)
-        return ERROR_EXTERNAL_ID, err
+        return nil, err
     }
     request.Header.Set("Authorization", "Bearer " + token)
 
@@ -86,25 +87,50 @@ func HandleUser(newUser *models.User, oldUser *models.User) (ExternalId, error) 
     resp, err := client.Do(request)
     if err != nil {
         log.Printf("Error: %s\n", err)
-        return ERROR_EXTERNAL_ID, err
+        return nil, err
     }
 
     resBody, err := io.ReadAll(resp.Body)
-    if err != nil { log.Printf("Error: %s\n", err)
-        return ERROR_EXTERNAL_ID, err
+    if err != nil { 
+        log.Printf("Error: %s\n", err)
+        return nil, err
     }
 
-    fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", resp.StatusCode)
-    fmt.Printf("client: response: %s\n", resBody)
+    return resBody, nil;
+}
 
+func HandleUser(newUser *models.User, oldUser *models.User) (ExternalId, error) {
+    token, err := getToken()
+    if err != nil {
+        log.Printf("Error: %s\n", err)
+        return ERROR_EXTERNAL_ID, err
+    }
+    // log.Printf("Got token: '%s'\n", token)
+    requestURL := fmt.Sprintf("%s/users", API_URL)
+
+
+    body, err := makeRequest(token, requestURL, http.MethodGet)
+    if err != nil {
+        log.Printf("Error: %s\n", err)
+        return ERROR_EXTERNAL_ID, err
+    }
     var getUsersResp GetUsersResponse
-    err = json.Unmarshal(resBody, &getUsersResp)
-    if err != nil { log.Printf("Error: %s\n", err)
+    err = json.Unmarshal(body, &getUsersResp)
+    if err != nil {
+        log.Printf("Error: %s\n", err)
         return ERROR_EXTERNAL_ID, err
     }
 
-    fmt.Printf("json: %v\n", getUsersResp)
-    // var request = http.NewRequest("GET", "http://localhost:59628/scim/users")
+    fmt.Printf("GotUsers:\n %s\n", structAsString(getUsersResp))
+
     return ERROR_EXTERNAL_ID, fmt.Errorf("error");
+}
+
+func structAsString(model any) string {
+    empJSON, err := json.MarshalIndent(model, "", " ")
+    if err != nil {
+        return "Something went wrong in structAsString"
+    }
+    str := fmt.Sprintf("%s", string(empJSON))
+    return str
 }
